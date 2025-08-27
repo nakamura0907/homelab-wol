2379502a5e0d109c9ba485270a0570f9143b88be
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2025-08-28 08:51:21 +0900

    ファイル分割

--- homelab_wol/src/main.rs
+++ homelab_wol/src/main.rs
@@ -1,6 +1,9 @@
-use std::{error::Error, net::UdpSocket};
+use std::error::Error;
 
 mod cli;
+mod wol;
+
+use crate::wol::send_magic_packet;
 
 use crate::cli::Options;
 
@@ -19,34 +22,4 @@ fn run(options: Options) -> Result<(), Box<dyn Error>> {
     send_magic_packet(options.mac_address)?;
 
     Ok(())
-}
-
-fn send_magic_packet(mac_address: String) -> Result<(), Box<dyn Error>> {
-    let mac_bytes = parse_mac_address(&mac_address);
-    let packet = create_magic_packet(&mac_bytes);
-
-    let socket = UdpSocket::bind("0.0.0.0:0")?;
-    socket.set_broadcast(true)?;
-
-    socket.send_to(&packet, "255.255.255.255:9")?;
-
-    Ok(())
-}
-
-fn create_magic_packet(mac_bytes: &[u8; 6]) -> [u8; 102] {
-    let mut packet = [0xFFu8; 102];
-
-    for i in 0..16 {
-        packet[6 + i * 6..6 + (i + 1) * 6].copy_from_slice(mac_bytes);
-    }
-
-    packet
-}
-
-fn parse_mac_address(mac_address: &str) -> [u8; 6] {
-    let bytes: Vec<u8> = mac_address
-        .split(':')
-        .map(|s| u8::from_str_radix(s, 16).unwrap_or(0))
-        .collect();
-    bytes.try_into().unwrap_or([0; 6])
 }
---
+++ homelab_wol/src/wol.rs
@@ -0,0 +1,65 @@
+use std::error::Error;
+use std::net::UdpSocket;
+
+pub fn send_magic_packet(mac_address: String) -> Result<(), Box<dyn Error>> {
+    let mac_bytes = parse_mac_address(&mac_address);
+    let packet = create_magic_packet(&mac_bytes);
+
+    let socket = UdpSocket::bind("0.0.0.0:0")?;
+    socket.set_broadcast(true)?;
+
+    socket.send_to(&packet, "255.255.255.255:9")?;
+
+    Ok(())
+}
+
+fn create_magic_packet(mac_bytes: &[u8; 6]) -> [u8; 102] {
+    let mut packet = [0xFFu8; 102];
+
+    for i in 0..16 {
+        packet[6 + i * 6..6 + (i + 1) * 6].copy_from_slice(mac_bytes);
+    }
+
+    packet
+}
+
+fn parse_mac_address(mac_address: &str) -> [u8; 6] {
+    let bytes: Vec<u8> = mac_address
+        .split(':')
+        .map(|s| u8::from_str_radix(s, 16).unwrap_or(0))
+        .collect();
+    bytes.try_into().unwrap_or([0; 6])
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_mac_address_valid() {
+        let mac = "00:11:22:33:44:55";
+        let bytes = parse_mac_address(mac);
+        assert_eq!(bytes, [0x00, 0x11, 0x22, 0x33, 0x44, 0x55]);
+    }
+
+    #[test]
+    fn test_parse_mac_address_invalid() {
+        let mac = "invalid";
+        let bytes = parse_mac_address(mac);
+        assert_eq!(bytes, [0, 0, 0, 0, 0, 0]);
+    }
+
+    #[test]
+    fn test_create_magic_packet_structure() {
+        let mac = [0x00, 0x11, 0x22, 0x33, 0x44, 0x55];
+        let packet = create_magic_packet(&mac);
+
+        assert!(packet[..6].iter().all(|&b| b == 0xFF));
+
+        for i in 0..16 {
+            let start = 6 + i * 6;
+            let end = start + 6;
+            assert_eq!(&packet[start..end], &mac);
+        }
+    }
+}
