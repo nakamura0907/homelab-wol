8dfd30540912053e8b0a31338bdefff353680b03
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2025-08-28 08:59:38 +0900

    文字数のテストを追加

--- homelab_wol/src/wol.rs
+++ homelab_wol/src/wol.rs
@@ -38,22 +38,28 @@ mod tests {
     #[test]
     fn test_parse_mac_address_valid() {
         let mac = "00:11:22:33:44:55";
+
         let bytes = parse_mac_address(mac);
+
         assert_eq!(bytes, [0x00, 0x11, 0x22, 0x33, 0x44, 0x55]);
     }
 
     #[test]
     fn test_parse_mac_address_invalid() {
         let mac = "invalid";
+
         let bytes = parse_mac_address(mac);
+
         assert_eq!(bytes, [0, 0, 0, 0, 0, 0]);
     }
 
     #[test]
     fn test_create_magic_packet_structure() {
         let mac = [0x00, 0x11, 0x22, 0x33, 0x44, 0x55];
+
         let packet = create_magic_packet(&mac);
 
+        assert_eq!(packet.len(), 102);
         assert!(packet[..6].iter().all(|&b| b == 0xFF));
 
         for i in 0..16 {
